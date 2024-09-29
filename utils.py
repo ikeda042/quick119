3ea3cd7eb212069c84d03e6a518b7229aab2efbc
ikeda042 <ikeda042main@gmail.com>
2024-09-30 00:01:11 +0900

    async ->

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup as BS
 from dataclasses import dataclass
+from concurrent.futures import ThreadPoolExecutor
 
 
 @dataclass
@@ -25,7 +26,7 @@ class Response:
         return f"Response({self.question})"
 
 
-def get_response(url: str):
+def get_response_sync(url: str):
     session = HTMLSession()
     response = session.get(url)
     response.html.render()
@@ -37,3 +38,19 @@ def get_response(url: str):
         for option in latest_article.select("a[data-bkey='goto']")
     ]
     return Response(html_raw, Question(res.select(".subtitle")[-1].text, options))
+
+
+async def get_response(url: str):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as pool:
+        return await loop.run_in_executor(pool, get_response_sync, url)
+
+
+import asyncio
+
+base_url = "https://www.fdma.go.jp/relocation/neuter/topics/filedList9_6/kyukyu_app/kyukyu_app_web/index.html?A000Q0105,A811"
+
+
+async def main():
+    response = await get_response(base_url)
+    print(response)
